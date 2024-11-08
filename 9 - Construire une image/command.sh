47b8d249d39aac1b9f441bdb15aa756fbef10cb8
Nathan Martel <nathan.martel@etu.mines-ales.fr>
2024-11-08 23:09:05 +0100

    NULL

--- 9 - Construire une image/command.sh
+++ 9 - Construire une image/command.sh
@@ -1,2 +1,37 @@
-#!/bin/bash
+# 9 - Construire une image
 
+> Source : [https://blog.microlinux.fr/formation-docker-09-construire/](https://blog.microlinux.fr/formation-docker-09-construire/)
+
+## Commandes :
+
+```bash
+
+
+```
+
+
+
+## Exercice 1 : 
+
+```bash
+
+
+```
+
+## Exercice 2 : 
+
+```bash
+
+
+```
+
+
+
+
+## Exercice 3 :
+
+
+```bash
+
+
+```
